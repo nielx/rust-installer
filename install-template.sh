0e06f0e967be01ae8bd26efa443682e8c3b4296d
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2015-03-09 22:27:02 +0100

    Haiku: detect OS

--- install-template.sh
+++ install-template.sh
@@ -363,6 +363,10 @@ get_host_triple() {
             _ostype=pc-windows-gnu
             ;;
 
+	Haiku*)
+            _ostype=unknown-haiku
+            ;;
+
 	*)
 	    err "unknown value from uname -s: $uname_value"
 	    ;;
